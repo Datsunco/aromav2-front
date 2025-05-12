4f3938aeec69e6ca2407f56b97eb081b50be664a
DatsunCo <danya200375d@gmail.com>
2025-05-12 13:40:35 +0300

    fix(about page)

--- src/app/[countryCode]/(main)/about-us/page.tsx
+++ src/app/[countryCode]/(main)/about-us/page.tsx
@@ -21,7 +21,10 @@ export default async function AboutUs(props: {
       <div className="max-w-5xl flex flex-col mx-auto ">
         <Header />
       </div>
-      <div className="mt-20">
+      <div className="mt-6 md:mt-20 max-w-6xl mx-auto">
+        <h1 className="px-4 font-acrom font-semibold mb-6 md:mb-20 text-4xl md:text-5xl">
+          О студии
+        </h1>
         <AboutSection />
       </div>
       <ReviewCarousel />
