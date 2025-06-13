f32b54a8633784182acc11e40bdddd56ac82a80a
DatsunCo <danya200375d@gmail.com>
2025-06-13 17:56:38 +0300

    fix(adpot)

--- src/modules/home/components/promo/index.tsx
+++ src/modules/home/components/promo/index.tsx
@@ -3,10 +3,21 @@
 import { getBannerData } from "@lib/data/banner"
 import { Button } from "@medusajs/ui"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 import { Banner } from "types/banner"
 
-const Promo = async () => {
-  const banner = await getBannerData()
+const Promo = () => {
+  const [banner, setBanner] = useState<Banner | null>(null)
+
+  useEffect(() => {
+    const getData = async () => {
+      const banner = await getBannerData()
+      setBanner(banner)
+    }
+
+    getData()
+  }, [])
+
   return (
     <div className="sticky top-0 h-14 bg-[#d9ff79] lg:rounded-[40px] lg:mx-1 lg:mt-1 flex justify-between items-center px-6 md:px-20 z-30">
       <span className="text-base md:text-2xl font-medium font-acrom">
